170a0646e8df0b6af6bb33a8056de6d224fe14b3
hpgy <hpougy@gmail.com>
2023-10-19 10:00:40 -0300

    filtrando casamentos sp

--- dash/etl/transform.py
+++ dash/etl/transform.py
@@ -10,6 +10,9 @@ class Transformer:
         'codmunreg' : 'municipio_realiz_casamento'
     }
 
+    codigo_ibge_estado_sp = 35
+    codigo_ibge_cidade_sp = 3550308
+
     def __init__(self):
 
         self.extract = Extractor()
@@ -23,3 +26,12 @@ class Transformer:
 
         return df.rename(self.colunas_mapper, axis=1)
     
+
+    def filtrar_casamentos_paulistanos(self, df:pd.DataFrame)->pd.DataFrame:
+
+        filtro = ((df['cod_residencia_cj1']==self.codigo_ibge_cidade_sp)|
+                  (df['cod_residencia_cj2']==self.codigo_ibge_cidade_sp))
+        
+        df_filtrado = df[filtro].copy().reset_index(drop=True)
+
+        return df_filtrado
