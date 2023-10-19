6f4137198bd5c893ea67bafc387676b46ca69951
hpgy <hpougy@gmail.com>
2023-10-19 09:44:17 -0300

    refactor: annotations e callable

--- dash/etl/extract.py
+++ dash/etl/extract.py
@@ -1,17 +1,20 @@
 from core.ckan_api_reader import Ckan
 from config import CKAN_DOMAIN
-
+from typing import Generator
 
 class Extractor:
+    '''Extrai os recursos de microdados de casamentos.
+    Retorna um generator de Dataframes'''
 
     pkg_name = 'microdados-casamentos'
 
-    def __init__(self, domain=CKAN_DOMAIN):
+    def __init__(self, domain:str=CKAN_DOMAIN)->None:
 
         self.ckan = Ckan(domain)
 
     
-    def list_resources(self):
+    def list_resources(self)->Generator:
+        
 
         regex_resource = 'Microdados de casamentos ocorridos nos municípios do Estado de São Paulo - \d{4}'
         
@@ -25,3 +28,8 @@ class Extractor:
                          parser_params={'sep' : ';'})
         
         return resources
+
+
+    def __call__(self)->Generator:
+
+        return self.list_resources()
