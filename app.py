b6129a7f4f0bed766ad6546083f968cbb38cea67
hpgy <hpougy@gmail.com>
2023-11-14 11:15:29 -0300

    funcionalidade graficos

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import dash
 import dash_deck
-import dash_html_components as html
+from dash import dcc, html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import plotly.express as px
 
 import pydeck as pdk
 import pandas as pd
@@ -20,32 +22,87 @@ total_casamentos = calc_total_mun()
 df_municipios = municipios()
 
 
+def get_opcoes_municipios(df_municipios):
+
+    filtrado = df_municipios[['cod_municipio', 'nome_municipio']]
+    renomeado = filtrado.rename({'nome_municipio' : 'label',
+                                'cod_municipio' : 'value'}, axis=1)
+    sem_duplicados = renomeado.drop_duplicates()
+
+    opcoes_municipios = sem_duplicados.to_dict(orient='records')
+
+    return opcoes_municipios
+
+opcoes_municipios = get_opcoes_municipios(df_municipios)
+
 r = gerar_pydeck(total_casamentos)
 
 
 app = dash.Dash(__name__)
 TOOLTIP_TEXT = {"html": "{nome_municipio_destino} : {total_casamentos}"}
 
-app.layout = html.Div(
-    [
-        html.Div(
+
+mapa_div = html.Div(
             dash_deck.DeckGL(
                 r.to_json(), id="deck-gl", mapboxKey=MAPBOX_ACCESS_TOKEN, tooltip=TOOLTIP_TEXT, enableEvents=['click']
             ),
             style={"height": "400px", "width": "100%", "position": "relative"},
-        ),
-        html.Div(
-            html.Pre(
-                id='output_data'
-            )
         )
+
+controls = dbc.Card(
+    [
+        html.Div(
+            [
+                dbc.Label("Nome do Município"),
+                dcc.Dropdown(
+                    id="seletor_mun",
+                    options=opcoes_municipios,
+                    value=3500105,
+                ),
+            ]
+        ),
+    ],
+    body=True,
+)
+
+
+div_graficos = dbc.Card(
+    [
+        dcc.Graph(id="grafico_linha_hab"),
+    ],
+    body=True,
+)
+
+
+app.layout = html.Div(
+    [
+        mapa_div,
+        controls,
+        div_graficos
+    
     ]
 )
 
 
-@app.callback(Output('output_data', "children"), Input("deck-gl", 'clickInfo'))
-def dump_json(data):
-    return json.dumps(data, indent=2)
+@app.callback(Output('seletor_mun', "value"), Input("deck-gl", 'clickInfo'))
+def get_mun_name(data):
+    
+    if data:
+        #puxa o municipio, caso tenha clicado fora já define como miss
+        objeto = data.get('object') or {'destino' : 'MISS'}
+        
+        return objeto.get('destino', 'MISS')
+    return 'MISS'
+    
+@app.callback(
+    Output("grafico_linha_hab", "figure"), 
+    Input("seletor_mun", "value"))
+def update_line_chart(cod_mun):
+
+    mask = df_municipios['cod_municipio']==cod_mun
+    fig = px.line(df_municipios[mask], 
+        x="Ano", y="habitantes_do_mun")
+    return fig
 
 
 if __name__ == "__main__":
