5762d32996dd8f34c0a0e615d3289eaf7e6edf22
hpgy <hpougy@gmail.com>
2023-11-14 10:12:36 -0300

    fixing problema com o tile layer e tootltip

--- app.py
+++ app.py
@@ -6,27 +6,27 @@ import pandas as pd
 
 
 from etls.scripts.casamentos import etl as casamamentos
+from etls.scripts.casamentos import TransformTotalMun
 from etls.scripts.municipios import etl as municipios
 from dash_exemplo.mapa_arco import gerar_pydeck
-
-
+from config import MAPBOX_ACCESS_TOKEN
 
 
 df_casamentos = casamamentos()
+calc_total_mun = TransformTotalMun(df_casamentos)
+total_casamentos = calc_total_mun()
 df_municipios = municipios()
 
 
-r = gerar_pydeck(df_casamentos.fillna(0))
-
-
-
+r = gerar_pydeck(total_casamentos)
 
 
 app = dash.Dash(__name__)
+TOOLTIP_TEXT = {"html": "{nome_municipio_destino} : {total_casamentos}"}
 
 app.layout = html.Div(
     dash_deck.DeckGL(
-        r.to_json(), id="deck-gl"
+        r.to_json(), id="deck-gl", mapboxKey=MAPBOX_ACCESS_TOKEN, tooltip=TOOLTIP_TEXT
     )
 )
 
