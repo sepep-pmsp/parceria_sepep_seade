42ebd6dc8870b9be0576d96c351cb4f195097486
hpgy <hpougy@gmail.com>
2023-11-14 10:44:14 -0300

    capturando click mapa

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import dash
 import dash_deck
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
 import pydeck as pdk
 import pandas as pd
-
+import json
 
 from etls.scripts.casamentos import etl as casamamentos
 from etls.scripts.casamentos import TransformTotalMun
@@ -25,10 +27,25 @@ app = dash.Dash(__name__)
 TOOLTIP_TEXT = {"html": "{nome_municipio_destino} : {total_casamentos}"}
 
 app.layout = html.Div(
-    dash_deck.DeckGL(
-        r.to_json(), id="deck-gl", mapboxKey=MAPBOX_ACCESS_TOKEN, tooltip=TOOLTIP_TEXT
-    )
+    [
+        html.Div(
+            dash_deck.DeckGL(
+                r.to_json(), id="deck-gl", mapboxKey=MAPBOX_ACCESS_TOKEN, tooltip=TOOLTIP_TEXT, enableEvents=['click']
+            ),
+            style={"height": "400px", "width": "100%", "position": "relative"},
+        ),
+        html.Div(
+            html.Pre(
+                id='output_data'
+            )
+        )
+    ]
 )
+
+
+@app.callback(Output('output_data', "children"), Input("deck-gl", 'clickInfo'))
+def dump_json(data):
+    return json.dumps(data, indent=2)
 
 
 if __name__ == "__main__":
