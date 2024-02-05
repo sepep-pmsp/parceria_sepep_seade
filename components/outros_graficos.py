36cedb3ea0d6bf03c83d01a6c021fdb164cdf3eb
Bruno Azuma Balzano <brunoazuma@usp.br>
2024-02-05 17:14:43 +0000

    Ajustado carregamento inicial dos graficos

--- components/outros_graficos.py
+++ components/outros_graficos.py
@@ -1,25 +1,27 @@
 from dash import dcc, html, dash_table
 import dash_bootstrap_components as dbc
-
-
-
+ 
+ 
+ 
 class Outros_graficos:
     def __init__(self):
-
-        self.outros_graficos_card = html.Div([dcc.Graph(id="grafico_linha_nascidos_vivos", className='nasc_graph'), dcc.Graph(id="grafico_linha_pib", className='pib_graph')])
-        
-
+       
+       
+        self.fig = dict(layout=dict(template ='plotly_dark', plot_bgcolor='rgba(0, 0, 0, 0)', paper_bgcolor='rgba(0, 0, 0, 0)', font_family="Roboto Mono" ))
+        self.outros_graficos_card = html.Div([dcc.Graph(id="grafico_linha_nascidos_vivos", className='nasc_graph', figure=self.fig), dcc.Graph(id="grafico_linha_pib", className='pib_graph', figure=self.fig)])
+       
+ 
     def criar_componente_final(self):
         outros_graficos_div = html.Div([self.outros_graficos_card],
                                      className='graph')
         return outros_graficos_div
-        
+       
     def pipeline(self):
-        
+       
         outros_graficos = self.criar_componente_final()
-
+ 
         return outros_graficos
-    
+   
     def __call__(self)-> html.Div:
-
+ 
         return self.pipeline()
